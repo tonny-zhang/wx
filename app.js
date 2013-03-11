8b768da47a7ab8bc12c2fa12bf98ee9277963830
zk <wodexintiao@gmail.com>
2013-03-11 15:23:51 +0800

    a

--- app.js
+++ app.js
@@ -8,54 +8,51 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
 	var response = function(code,msg){
-		log(reqMethod,req.url);
 		res.writeHead(code,{'Content-Type':'text/html'});
 		res.end(msg);
 	}
 	var reqMethod = req.method.toUpperCase();
-	var isForbid = true;
-	
-	if(reqMethod == 'GET'){
-		if(~req.url.indexOf('?')){
-			var params = url.parse(req.url,true).query;
-			var sha1Str = [params.nonce,params.timestamp,'tonnyzhang'].sort().join('');
+	var reqUrl = req.url;
+	if(reqUrl == '/log.txt'){
+		fs.readFile('./log.txt',function(err,data){
+			response(200,(err||data).toString());
+		});		
+	}else{
+		//记录日志		
+		log(reqMethod,reqUrl);
+		var params = url.parse(reqUrl,true).query;
+		var sha1Str = [params.nonce,params.timestamp,'tonnyzhang'].sort().join('');
 
-			var signature = crypto.createHash('sha1').update(sha1Str).digest('hex');
-			
-			if(params.signature == signature){
+		var signature = crypto.createHash('sha1').update(sha1Str).digest('hex');
+		
+		if(params.signature == signature){
+			if(reqMethod == 'GET'){
 				response(200,params.echostr);
-				isForbid = false;
+			}else{
+				var message = '';
+				req.on('data',function(d){
+					message = d.toString();
+				});
+				req.on('end',function(){
+					xml.parse2Json(message,function(err,result){
+						log('POST_XML',JSON.stringify(err||result));
+						if(!err){
+							var weiXin = new WeiXin(result);
+							switch(result.MsgType){
+								case 'text':
+									break;
+								default:
+							}
+							var res_xml = weiXin.textTmpl('这是一个测试');
+							response(200,res_xml);
+						}else{
+							response(500,'parse xml error!');
+						}
+					})
+				});
 			}
+		}else{
+			response(200,'no weiXin');
 		}
-		if(req.url == '/log.txt'){
-			var content = fs.readFileSync('./log.txt');
-			response(200,content);
-			isForbid = false;
-		}
-	}else if(reqMethod == 'POST'){
-		var message = '';
-		req.on('data',function(d){
-			message = d.toString();
-		});
-		req.on('end',function(){
-			xml.parse2Json(message,function(err,result){
-				if(!err){
-					var weiXin = new WeiXin(result);
-					switch(result.MsgType){
-						case 'text':
-							break;
-						default:
-					}
-					var res_xml = weiXin.textTmpl('这是一个测试');
-					response(200,res_xml);
-				}else{
-					response(500,'parse xml error!');
-				}
-			})
-		});
-		isForbid = false;
-	}
-	if(isForbid){
-		response(403,'something is wrong!');
 	}
 }).listen(process.env.PORT || 5000, null);
