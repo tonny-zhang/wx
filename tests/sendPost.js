28a8acc0b1254a70a5bbb798a4626ca4321b8e0f
zk <wodexintiao@gmail.com>
2013-03-26 09:59:56 +0800

    a

--- tests/sendPost.js
+++ tests/sendPost.js
@@ -124,6 +124,13 @@ str = '<xml><ToUserName><![CDATA[gh_8f47ec7c055d]]></ToUserName>\n'+
 			'<MsgType><![CDATA[text]]></MsgType>\n'+
 			'<Content><![CDATA[朝阳区天气]]></Content>\n'+
 			'<MsgId>5853982309245517843</MsgId>\n'+
-			'</xml>'  												
+			'</xml>'  	
+str = '<xml><ToUserName><![CDATA[gh_8f47ec7c055d]]></ToUserName>\n'+
+			'<FromUserName><![CDATA[o7fAGj-j4y-Ey5nvTTE1Z9wwyCY4]]></FromUserName>\n'+
+			'<CreateTime>1362986469</CreateTime>\n'+
+			'<MsgType><![CDATA[text]]></MsgType>\n'+
+			'<Content><![CDATA[河南信阳天气]]></Content>\n'+
+			'<MsgId>5853982309245517843</MsgId>\n'+
+			'</xml>' 														
 req.write(str);
 req.end();
