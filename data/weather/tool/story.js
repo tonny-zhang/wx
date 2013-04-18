86c284b052af5797f862c6f1efd79d1ba180f054
zk <wodexintiao@gmail.com>
2013-04-18 15:51:43 +0800

    optimize cache all date

--- data/weather/tool/story.js
+++ data/weather/tool/story.js
@@ -5,7 +5,7 @@ var ALLCODE_CACHE_FILE = __dirname+'/../cache/allCode.json';
 
 function rewriteAllCodeCache(callback){
 	var allCode = JSON.parse(fs.readFileSync(ALLCODE_CACHE_FILE));
-	allCode = allCode.slice(0,5);
+	// allCode = allCode.slice(0,5);
 	var totalLen = allCode.length;
 	var execNum = 0;
 	var cb = function(){
